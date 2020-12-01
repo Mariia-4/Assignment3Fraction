7beca6d1d654efcd56cb1196ccf5e57fa136b9ea
freddy-lopez <74562226+freddy-lopez@users.noreply.github.com>
2020-12-01 11:17:02 -0800

    Add files via upload

---
+++ Assignment3Fraction/fraction/FractionTestJupiter.java
@@ -0,0 +1,14 @@
+package fraction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FractionTestJupiter {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Assignment3Fraction/fraction/IllegalArgumentJupiter.java
@@ -0,0 +1,14 @@
+package fraction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IllegalArgumentJupiter {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
